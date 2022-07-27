db2d1eef8a605a7846ce1bce1d139b504bd01f15
Danil <mydogfromhell666@gmail.com>
2022-07-27 12:51:10 +0300

    add like/dislike visuals

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import './Item.css'
+import React, {useState} from 'react';
+import './Item.css';
+import like from './like.svg'
+import empty from './empty.svg'
 
 const Item = ({ picture, wand, alive, gender, house, nameTitle, actor }) => {
+
+    const [isLike, setLike] = useState(false);
+
     return (
         <div className='card' >
             <div className="card__picture">
                 <img className="card__img" width='334' height='192' src={picture} alt=""/>
             </div>
             <div className="like__container">
-                <svg className="like__img empty">\n' +
-                '  <use href="#empty-like"></use>\n' +
-                "</svg>
+                <button onClick={() => {setLike(!isLike)}}>
+                    <img className='like__img' src={isLike ? like : empty} alt="like"/>
+                </button>
             </div>
             <div className="info">
                 <h2 className = "info__title">{nameTitle}</h2>
